7ff3961cf600c2efbdb5b15cf2015c123785cf01
yodapancake <maximushirsch@gmail.com>
2021-10-22 19:58:55 -0700

    yes

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let icon = document.getElementsByClassName("icon")[0];
+
+icon.addEventListener('click', responsive_control);
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function responsive_control() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
